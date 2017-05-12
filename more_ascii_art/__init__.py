be445542373397edb755f6a134f92e1d6a712ad6
Shreyas Lad <shadowtemplates@gmail.com>
2017-05-12 16:19:48 -0700

    Added more stuff

--- more_ascii_art/__init__.py
+++ more_ascii_art/__init__.py
@@ -1,3 +1,11 @@
+# Importing assets
+import cat_left
+import cat_right
+
+# Error handling variable
+err = print("Your selection hasn't worked")
+
+# Help section
 def help():
     print("     Welcome to more_ascii_art help!      ")
     print("Usage: ")
@@ -10,3 +18,21 @@ def help():
     print("Upcoming Features: ")
     print("       1. Support for PyPI")
     print("       2. A CAT")
+
+# User selection and processing    
+def selection():
+    print("Please note that selections are case sensitive...")
+    print("Here are the selections you can choose from: ")
+    print("1. cat_left ")
+    print("2. cat_right ")
+    user_input = input("Please type in your selection: ")
+    
+    if (user_input == "cat_left"):
+        print cat_left()
+    else:
+        print err
+        
+    if (user_input == "cat_right"):
+        print cat_right()
+    else:
+        print err
