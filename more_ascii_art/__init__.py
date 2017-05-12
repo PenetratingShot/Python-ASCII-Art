7d3be715969cd734751592a8c1b07ba8a3633ae2
Shreyas Lad <shadowtemplates@gmail.com>
2017-05-12 16:29:21 -0700

    Updated help

--- more_ascii_art/__init__.py
+++ more_ascii_art/__init__.py
@@ -3,21 +3,19 @@ import cat_left
 import cat_right
 
 # Error handling variable
-err = print("Your selection hasn't worked")
+err = print("Something went wrong. Report a bug at https://github.com/PenetratingShot/Python-ASCII-Art/issues")
 
 # Help section
 def help():
     print("     Welcome to more_ascii_art help!      ")
     print("Usage: ")
-    print("       1. Build the library with $ pip install -e .")
-    print("       2. Use the python shell to $ import more_ascii_art")
+    print("       1. Build the library with >>> pip install -e .")
+    print("       2. Use the python shell to >>> import more_ascii_art")
     print("       3. Type in print and then the build name and then the function you wish to execute")
-    print("            Like this: $ print more_ascii_art.help()")
+    print("            Like this: >>> print more_ascii_art.help()")
     print("Commands: ")
     print("       1. help() - Displays this help message")
-    print("Upcoming Features: ")
-    print("       1. Support for PyPI")
-    print("       2. A CAT")
+    print("       2. selection() - Displays all ASCII selections and prompts you for which one you want")
 
 # User selection and processing    
 def selection():
