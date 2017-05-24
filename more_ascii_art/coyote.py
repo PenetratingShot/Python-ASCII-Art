0df94f41e39e25bbda9eab1f7d09e2ab1ae5579c
Shreyas Lad <shadowtemplates@gmail.com>
2017-05-24 10:47:18 -0700

    coyote

--- more_ascii_art/coyote.py
+++ more_ascii_art/coyote.py
@@ -1,1 +1,8 @@
+# drawing a coyote
 
+def coyote():
+  print("        __'''''__  ")
+  print("     .-'         '-. ")
+  print("   .'               '.  ")
+  print("  /                   \  ")
+  print(" /        |\           \  ")
