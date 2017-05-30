2ca76c8b9ddf1efe65db5fc3973602174d90e99f
Shreyas Lad <shadowtemplates@gmail.com>
2017-05-30 11:23:48 -0700

    Update __init__.py

--- more_ascii_art/__init__.py
+++ more_ascii_art/__init__.py
@@ -15,10 +15,12 @@ err = print("Something went wrong. Report a bug at https://github.com/Penetratin
 def help():
     print("     Welcome to more-ascii-art help!      ")
     print("Usage: (local) ")
-    print("       1. Build the library with >>> pip install -e .")
-    print("       2. Use the python shell to >>> import more-ascii-art")
-    print("       3. Type in print and then the build name and then the function you wish to execute")
-    print("            Like this: >>> print more-ascii-art.help()")
+    print("       1. Clone with git... $ git clone https://github.com/PenetratingShot/Python-ASCII-Art ")
+    print("       2. Build the library with >>> pip install -e .")
+    print("       3. Use the python shell to >>> import more-ascii-art")
+    print("       4. Type in print and then the build name and then the function you wish to execute")
+    print("            Like this: >>> print more-ascii-art.help() ")
+    print("            Or this: >>> print more-ascii-art.selection() ")
     print("Usage: (pip package) ")
     print("       1. Install more-ascii-art with >>> pip install more-ascii-art ")
     print("       2. Type in 'print' and then the function you wish to execute... ")
@@ -35,9 +37,16 @@ def help():
     print("            g. duck ")
     print("            h. ghost ")
     print("       3. changelog() - Displays what has been changed in each version of more-ascii-art ")
-
+        
 # Changelog for different versions
 def changelog():
+    print("v1.4: ")
+    print(" - ghost picture ")
+    print(" - dragon picture ")
+    print(" - duck picture ")
+    print(" - guitar picture ")
+    print(" ")
+    print("  ")
     print("v1.3: ")
     print("  - elephant picture")
     print("  - updated help section ")
